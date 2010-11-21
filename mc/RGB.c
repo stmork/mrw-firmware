4ed645fe8e061b0d12a869ba1805eab3eb459379
Steffen A. Mork <linux-dev@morknet.de>
2010-11-21 21:25:51 +0000

    * Kleinere RGB-Spielerei...
    git-svn-id: svn+ssh://svn.code.sf.net/p/mrw/code/trunk@147 0b73ed94-99fe-4531-ab00-e2d4daa7083f

--- mc/RGB.c
+++ mc/RGB.c
@@ -1,6 +1,6 @@
 /*
 **
-**	$Filename:	test_light.c $
+**	$Filename:	RGB.c $
 **	$Revision$
 **	$Date$
 **	$Author$
@@ -28,21 +28,107 @@
 
 #include "config.h"
 #include "eeprom.h"
+#include "light.h"
 #include "mcp2515.h"
+#include "pwm.h"
+#include "serial.h"
 #include "sleep.h"
+#include "timer.h"
 
 #define NODE_READY     MCP_LED_GREEN
 
-#define B1 1
-#define R  2
-#define B2 4
-#define G  8
+#define INDEX_B1 3
+#define INDEX_R  2
+#define INDEX_B2 1
+#define INDEX_G  0
 
-static void init(void)
+ISR(TIMER1_COMPA_vect)
 {
-	DDRA  = 0xff;
-	PORTA = 0xee;
-	PINA  = 0;
+	mrw_device *dvc = config.dvc;
+	uint8_t     i;
+
+	for (i = 0;i < config.count; i++)
+	{
+		handle_pwm(&dvc->unit.u_light);
+		dvc++;
+	}
+}
+
+static uint8_t counter;
+static uint8_t state;
+
+ISR(TIMER2_OVF_vect)
+{
+	uint8_t inv = ~counter;
+
+	switch(state & 7)
+	{
+	case 0: // R B1 hoch
+		set_dimm(&config.dvc[INDEX_R].unit.u_light, counter);
+		set_dimm(&config.dvc[INDEX_B1 + 4].unit.u_light, counter);
+		break;
+
+	case 1: // G R hoch
+		set_dimm(&config.dvc[INDEX_G].unit.u_light, counter);
+		set_dimm(&config.dvc[INDEX_R + 4].unit.u_light, counter);
+		break;
+
+	case 2: // R B1 runter
+		set_dimm(&config.dvc[INDEX_R].unit.u_light, inv);
+		set_dimm(&config.dvc[INDEX_B1 + 4].unit.u_light, inv);
+		break;
+
+	case 3: // B G hoch
+		set_dimm(&config.dvc[INDEX_B1].unit.u_light, counter);
+		set_dimm(&config.dvc[INDEX_G + 4].unit.u_light, counter);
+		break;
+
+	case 4: // G R runter
+		set_dimm(&config.dvc[INDEX_G].unit.u_light, inv);
+		set_dimm(&config.dvc[INDEX_R + 4].unit.u_light, inv);
+		break;
+
+	case 5: // R B2 hoch
+		set_dimm(&config.dvc[INDEX_R].unit.u_light, counter);
+		set_dimm(&config.dvc[INDEX_B2 + 4].unit.u_light, counter);
+		break;
+
+	case 6: // B G runter
+		set_dimm(&config.dvc[INDEX_B1].unit.u_light, inv);
+		set_dimm(&config.dvc[INDEX_G + 4].unit.u_light, inv);
+		break;
+
+	case 7: // R B2 runter
+		set_dimm(&config.dvc[INDEX_R].unit.u_light, inv);
+		set_dimm(&config.dvc[INDEX_B2 + 4].unit.u_light, inv);
+		break;
+	}
+
+	counter+=4;
+	if (counter == 0)
+	{
+		state++;
+	}
+}
+
+static void config_init(void)
+{
+	mrw_device *dvc = config.dvc;
+	uint8_t     i;
+
+	for (i = 0;i < 8;i++)
+	{
+		dvc->unit_no                = i + 1;
+		dvc->unit_type              = TYPE_LIGHT;
+		dvc->unit.u_light.threshold = 0;
+		dvc->unit.u_light.type      = 0;
+		config_connection(&dvc->unit.u_light.pin, i);
+
+		set_dimm  (&dvc->unit.u_light, 0);
+		light_init(&dvc->unit.u_light);
+		dvc++;
+	}
+	config.count = i;
 }
 
 int main(void)
@@ -52,7 +138,10 @@ int main(void)
 	/* Takt initialisieren, so dass MCP2515 seine 16 MHz auch her gibt. */
 	/* Ansonsten passiert hier heute per CAN-Bus nix ;-) */
 	mcp2515_init(config.id, 1, MCP2515_SINGLE_TX_BUFFER);
-	init();
+	config_init();
+	timer1_init(F_CPU / (50 * PWM_TABLE_SIZE));
+	timer2_init();
+	serial_init();
 	mcp2515_write_rx_output_pins(NODE_READY);
 
 	do
