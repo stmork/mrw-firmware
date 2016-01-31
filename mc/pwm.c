011b204c2b36b45606a729acb8d0892a76ebf3c0
Steffen A. Mork <linux-dev@morknet.de>
2016-01-31 12:52:23 +0000

    * Jahr aktualisiert.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mrw/code/trunk@255 0b73ed94-99fe-4531-ab00-e2d4daa7083f

--- mc/pwm.c
+++ mc/pwm.c
@@ -8,7 +8,7 @@
 **
 **	PWM soft handler
 **
-**	Copyright (C) 2010 committers of this modelrailway project. All rights reserved.
+**	Copyright (C) 2016 committers of this modelrailway project. All rights reserved.
 **
 **	This program and the accompanying materials are made available under the
 **	terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
